d44048dd54f96acf90768ed74ba69eafebdb2b23
Mikhail <m6453327@icloud.com>
2024-07-29 00:14:12 +0300

    huge commit

--- demo/src/main/java/com/example/Parser.java
+++ demo/src/main/java/com/example/Parser.java
@@ -1,49 +1,46 @@
 package com.example;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
-
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
-public class parseWeather {
+public class Parser {
 
-    static Document page;
+    Document page;
 
-    public parseWeather(String city) throws MalformedURLException, IOException {
-        parseWeather.page = get_page(city);
-    }
-
-    private static Document get_page(String city) throws MalformedURLException, IOException {
+    public Parser(String city) throws IOException  {
         String url = "https://pogoda.mail.ru/prognoz/" + city + "/14dney/";
-        Document page = Jsoup.connect(url).userAgent("Chrome/4.0.249.0 Safari/532.5").referrer("http://www.google.com").get();
-        return page;
+        page = Jsoup.connect(url).userAgent("Chrome/4.0.249.0 Safari/532.5").referrer("http://www.google.com").get();
     }
 
-    public static Elements get_dates() {
+    public Elements get_dates() {
         Elements dates = page.select("span[class=hdr__inner]");
         return dates;
     }
-    public static Elements get_times() {
+    public Elements get_times() {
         Elements timesOf24Hours = page.select("span[class=text text_block text_bold_normal text_fixed margin_bottom_10]");
         return timesOf24Hours;
     }
-    public static Elements get_temps() {
+    public Elements get_temps() {
         Elements temperatures = page.select("span[class=text text_block text_bold_medium margin_bottom_10]");
         return temperatures;
     }
-    public static Elements get_conditions() {
+    public Elements get_conditions() {
         Elements conditions = page.select("span[class=text text_block text_light_normal text_fixed]");
         return conditions;
     }
-    public static Elements get_feeleng() {
+    public Elements get_feeleng() {
         Elements feels = page.select("span[class=text text_block text_light_normal text_fixed color_gray]");
         return feels;
     }
-    public static Elements get_other() {
+    public Elements get_other() {
         Elements pressure = page.select("span[class=link__text]");
         return pressure;
     }
+
+    /*public static void main(String[] args) throws IOException {
+        System.out.println(new Parser("moskva").page.toString());
+    }*/
 }
--- demo/src/main/java/com/example/WeatherGUI.java
+++ demo/src/main/java/com/example/WeatherGUI.java
@@ -6,8 +6,7 @@ import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
-import java.net.MalformedURLException;
+
 
 public class WeatherGUI {
 
@@ -38,11 +37,7 @@ public class WeatherGUI {
         getWeatherButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                try {
-                    getWeatherData();
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                }
+                getWeatherData();
             }
         });
         panel.add(getWeatherButton);
@@ -53,71 +48,81 @@ public class WeatherGUI {
 
         JScrollPane scrollPane = new JScrollPane(weatherTable);
         frame.add(scrollPane, BorderLayout.CENTER);
+        frame.setVisible(true);
     }
 
-    private void getWeatherData() throws MalformedURLException, IOException {
+    private void getWeatherData() {
         String city = cityTextField.getText();
-        parseWeather parser = new parseWeather(city);
-        Weather weather = new Weather(parseWeather.get_dates(), parseWeather.get_times(), parseWeather.get_temps(), parseWeather.get_conditions(), parseWeather.get_feeleng(), parseWeather.get_other());
+        city = CityNames.getName(city);
+        if (city == null) {
+            ErrorDialog("City not found");
+        } else {
 
-        Object[][] data = new Object[126][5];
-        for (int i = 0; i < 14; i++) {
-            data[i * 9][0] = weather.getDay(i + 1).getDate();
-            data[i * 9 + 1][0] = "Temperature:";
-            data[i * 9 + 1][1] = weather.getDay(i + 1).getNight().getTemperature();
-            data[i * 9 + 1][2] = weather.getDay(i + 1).getMorning().getTemperature();
-            data[i * 9 + 1][3] = weather.getDay(i + 1).getNoon().getTemperature();
-            data[i * 9 + 1][4] = weather.getDay(i + 1).getEvening().getTemperature();
-            data[i * 9 + 2][0] = "Condition:";
-            data[i * 9 + 2][1] = weather.getDay(i + 1).getNight().getCondition();
-            data[i * 9 + 2][2] = weather.getDay(i + 1).getMorning().getCondition();
-            data[i * 9 + 2][3] = weather.getDay(i + 1).getNoon().getCondition();
-            data[i * 9 + 2][4] = weather.getDay(i + 1).getEvening().getCondition();
-            data[i * 9 + 3][0] = "Feeleng:";
-            data[i * 9 + 3][1] = weather.getDay(i + 1).getNight().getFeeling();
-            data[i * 9 + 3][2] = weather.getDay(i + 1).getMorning().getFeeling();
-            data[i * 9 + 3][3] = weather.getDay(i + 1).getNoon().getFeeling();
-            data[i * 9 + 3][4] = weather.getDay(i + 1).getEvening().getFeeling();
-            data[i * 9 + 4][0] = "Pressure:";
-            data[i * 9 + 4][1] = weather.getDay(i + 1).getNight().getPressure();
-            data[i * 9 + 4][2] = weather.getDay(i + 1).getMorning().getPressure();
-            data[i * 9 + 4][3] = weather.getDay(i + 1).getNoon().getPressure();
-            data[i * 9 + 4][4] = weather.getDay(i + 1).getEvening().getPressure();
-            data[i * 9 + 5][0] = "Humidity";
-            data[i * 9 + 5][1] = weather.getDay(i + 1).getNight().getHumidity();
-            data[i * 9 + 5][2] = weather.getDay(i + 1).getMorning().getHumidity();
-            data[i * 9 + 5][3] = weather.getDay(i + 1).getNoon().getHumidity();
-            data[i * 9 + 5][4] = weather.getDay(i + 1).getEvening().getHumidity();
-            data[i * 9 + 6][0] = "Wind:";
-            data[i * 9 + 6][1] = weather.getDay(i + 1).getNight().getWind();
-            data[i * 9 + 6][2] = weather.getDay(i + 1).getMorning().getWind();
-            data[i * 9 + 6][3] = weather.getDay(i + 1).getNoon().getWind();
-            data[i * 9 + 6][4] = weather.getDay(i + 1).getEvening().getWind();
-            data[i * 9 + 7][0] = "UV:";
-            data[i * 9 + 7][1] = weather.getDay(i + 1).getNight().getUv();
-            data[i * 9 + 7][2] = weather.getDay(i + 1).getMorning().getUv();
-            data[i * 9 + 7][3] = weather.getDay(i + 1).getNoon().getUv();
-            data[i * 9 + 7][4] = weather.getDay(i + 1).getEvening().getUv();
-            data[i * 9 + 8][0] = "Chance:";
-            data[i * 9 + 8][1] = weather.getDay(i + 1).getNight().getChanceOfPrecip();
-            data[i * 9 + 8][2] = weather.getDay(i + 1).getMorning().getChanceOfPrecip();
-            data[i * 9 + 8][3] = weather.getDay(i + 1).getNoon().getChanceOfPrecip();
-            data[i * 9 + 8][4] = weather.getDay(i + 1).getEvening().getChanceOfPrecip();
         
+        try {
+            Parser parser = new Parser(city);
+            Weather weather = new Weather(parser.get_dates(), parser.get_times(), parser.get_temps(), parser.get_conditions(), parser.get_feeleng(), parser.get_other());
+
+            Object[][] data = new Object[126][5];
+            for (int i = 0; i < 14; i++) {
+                data[i * 9][0] = weather.getDay(i + 1).getDate();
+                data[i * 9 + 1][0] = "Temperature:";
+                data[i * 9 + 1][1] = weather.getDay(i + 1).getNight().getTemperature();
+                data[i * 9 + 1][2] = weather.getDay(i + 1).getMorning().getTemperature();
+                data[i * 9 + 1][3] = weather.getDay(i + 1).getNoon().getTemperature();
+                data[i * 9 + 1][4] = weather.getDay(i + 1).getEvening().getTemperature();
+                data[i * 9 + 2][0] = "Condition:";
+                data[i * 9 + 2][1] = weather.getDay(i + 1).getNight().getCondition();
+                data[i * 9 + 2][2] = weather.getDay(i + 1).getMorning().getCondition();
+                data[i * 9 + 2][3] = weather.getDay(i + 1).getNoon().getCondition();
+                data[i * 9 + 2][4] = weather.getDay(i + 1).getEvening().getCondition();
+                data[i * 9 + 3][0] = "Feeleng:";
+                data[i * 9 + 3][1] = weather.getDay(i + 1).getNight().getFeeling();
+                data[i * 9 + 3][2] = weather.getDay(i + 1).getMorning().getFeeling();
+                data[i * 9 + 3][3] = weather.getDay(i + 1).getNoon().getFeeling();
+                data[i * 9 + 3][4] = weather.getDay(i + 1).getEvening().getFeeling();
+                data[i * 9 + 4][0] = "Pressure:";
+                data[i * 9 + 4][1] = weather.getDay(i + 1).getNight().getPressure();
+                data[i * 9 + 4][2] = weather.getDay(i + 1).getMorning().getPressure();
+                data[i * 9 + 4][3] = weather.getDay(i + 1).getNoon().getPressure();
+                data[i * 9 + 4][4] = weather.getDay(i + 1).getEvening().getPressure();
+                data[i * 9 + 5][0] = "Humidity";
+                data[i * 9 + 5][1] = weather.getDay(i + 1).getNight().getHumidity();
+                data[i * 9 + 5][2] = weather.getDay(i + 1).getMorning().getHumidity();
+                data[i * 9 + 5][3] = weather.getDay(i + 1).getNoon().getHumidity();
+                data[i * 9 + 5][4] = weather.getDay(i + 1).getEvening().getHumidity();
+                data[i * 9 + 6][0] = "Wind:";
+                data[i * 9 + 6][1] = weather.getDay(i + 1).getNight().getWind();
+                data[i * 9 + 6][2] = weather.getDay(i + 1).getMorning().getWind();
+                data[i * 9 + 6][3] = weather.getDay(i + 1).getNoon().getWind();
+                data[i * 9 + 6][4] = weather.getDay(i + 1).getEvening().getWind();
+                data[i * 9 + 7][0] = "UV:";
+                data[i * 9 + 7][1] = weather.getDay(i + 1).getNight().getUv();
+                data[i * 9 + 7][2] = weather.getDay(i + 1).getMorning().getUv();
+                data[i * 9 + 7][3] = weather.getDay(i + 1).getNoon().getUv();
+                data[i * 9 + 7][4] = weather.getDay(i + 1).getEvening().getUv();
+                data[i * 9 + 8][0] = "Chance:";
+                data[i * 9 + 8][1] = weather.getDay(i + 1).getNight().getChanceOfPrecip();
+                data[i * 9 + 8][2] = weather.getDay(i + 1).getMorning().getChanceOfPrecip();
+                data[i * 9 + 8][3] = weather.getDay(i + 1).getNoon().getChanceOfPrecip();
+                data[i * 9 + 8][4] = weather.getDay(i + 1).getEvening().getChanceOfPrecip();
+                
+            }
+            String[] columnNames = {"Date", "Night", "Morning", "Noon", "Evening"};
+            DefaultTableModel model = new DefaultTableModel(data, columnNames);
+            weatherTable.setModel(model);
+        } catch (Exception ex) {
+            ErrorDialog("Connection failed");
         }
-        String[] columnNames = {"Date", "Night", "Morning", "Noon", "Evening"};
-        DefaultTableModel model = new DefaultTableModel(data, columnNames);
-        weatherTable.setModel(model);
-        weatherTable.setModel(model);
+        }
+    }
+
+    public void ErrorDialog(String errorMessage) {
+        JOptionPane.showMessageDialog(frame, errorMessage, "Error", JOptionPane.ERROR_MESSAGE);
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                WeatherGUI window = new WeatherGUI();
-                window.frame.setVisible(true);
-            }
-        });
+        WeatherGUI gui = new WeatherGUI();
+        gui.initialize();
     }
 }
